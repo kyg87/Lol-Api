87746eccbdc9ae42071f7a0005b259460c2457ff
DESKTOP-28M1M60\dridy <dridy@naver.com>
2017-04-03 06:05:30 +0900

    sdsdsd

--- src/HelloLolApi.java
+++ src/HelloLolApi.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JOptionPane;
 
@@ -35,7 +41,54 @@ public class HelloLolApi {
 			System.out.println(summonerLevel);
 			
 			
+			URL revisionDateURL = new URL("https://kr.api.riotgames.com/api/lol/KR/v1.4/summoner/"+id+"?api_key=RGAPI-133cfc7a-22f3-4eef-83f5-2c7ca3d3a5d6");
+			
+			BufferedReader ina = new BufferedReader(new InputStreamReader(revisionDateURL.openStream()));
+			
+			String lolApiJsona = ina.readLine();
+			System.out.println(lolApiJsona);
+			
+			JSONObject lolApia = new JSONObject(lolApiJsona);
+			
+			String _id = Integer.toString(id);
+			long revisionDate = lolApia.getJSONObject(_id).getLong("revisionDate");
 	
+
+			String dateFormatStringTime;
+			
+			Date currentDate = new Date(System.currentTimeMillis());
+			Date date1 = new Date(revisionDate + 60*60*7*1000);
+
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+			dateFormatStringTime = dateFormat.format(currentDate);
+			System.out.println(dateFormatStringTime);
+			
+			dateFormatStringTime = dateFormat.format(date1);
+			System.out.println(dateFormatStringTime);
+		
+			
+			URL currentInGameURL = new URL("https://kr.api.riotgames.com/observer-mode/rest/consumer/getSpectatorGameInfo/KR/"+id+"?api_key=RGAPI-133cfc7a-22f3-4eef-83f5-2c7ca3d3a5d6");
+	
+			
+			BufferedReader ino = new BufferedReader(new InputStreamReader(currentInGameURL.openStream()));
+			
+			
+			String lolApiJsonao = ino.readLine();
+			
+			JSONObject lolApiao = new JSONObject(lolApiJsona);
+			System.out.println(lolApiJsonao);
+			//dateFormatStringTime = dateFormat.format(System.currentTimeMillis()-revisionDate + 60*60*7*1000);
+			
+	/*		long mills = System.currentTimeMillis() - revisionDate + 60*60*7*1000;
+			
+			
+			System.out.println((mills/1000)/60);*/
+			
+		
+			long gameStartTime = lolApiao.getLong("gameStartTime");
+			System.out.println(gameStartTime);
 	}
 
 }
